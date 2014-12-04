5d1cbfee7caa6b60dc75c8947915e310645619de
bjlinville1 <Jgrant@Jennifers-MacBook-Pro.local>
2014-12-03 21:32:02 -0500

    it seems that there is an issue comparing the string I pushed to an array and the values from our @items hash. I'm pushing to save what I have, show you where I've written puts(p) to find my error, and will now solve it

--- Checkout.rb
+++ Checkout.rb
@@ -8,7 +8,7 @@ class Item
 
   # Set defaults
   def initialize
-    puts "A new item was created!"
+    # puts "A new item was created!"
     self.sku = nil
     self.price = nil
   end
@@ -22,19 +22,51 @@ class Item
   def self.add_item(*item_attributes)
     item = Item.new
     item.sku = item_attributes[0]
-    puts "a new item was skud!"
+    # puts "a new item was skud!"
     item.price = item_attributes[1]
-    puts "a new item was priced!"
+    # puts "a new item was priced!"
     @items.push item
-    puts "a new item was pushed!"
+    # puts "a new item was pushed!"
   end
 
 
   Item.add_item("a", 50)
   Item.add_item("b", 30)
-  puts "items were added successfully"
+  Item.add_item("c", 20)
+  Item.add_item("d", 15)
+  # puts "items were added successfully"
   #Accept a list of items to be purchased
   # items = gets.to_a
   p @items
+
+
+puts "Please enter the items you want, with no spaces, or separation of any kind. For example: 'abc'.
+ If you want more than one of any item, please re-enter it, until you reach your desired quantity. For example: 'aaaaabc'."  
+
+user_items = gets.chomp
+
+# After checking my initial solution, I've found that the code on 7 digital's Readme is making use of a regular expression named .scan as well as .split. They are slightly cleaner, but using .chars to push the string to an array works just fine here. 
+
+user_items_characters = user_items.chars
+p user_items_characters
+
+#To Do: Build this array dynamically, so the number of elements is == to the number of items for sale, and elements both default and reset to 0 each time the program runs.
+
+subtotal = 0
+z = 0
+
+user_items_characters.each do |x| 
+  if @items[z].sku == x
+    @items[z].price += subtotal
+    p subtotal
+    z += 1
+  end
+end
+
+p subtotal
+# subtotal = 0
+# user_items_characters.each do |x|
+#   quantities[x]*user_items_characters[x[:price]] += subtotal
+# end
 
 end
